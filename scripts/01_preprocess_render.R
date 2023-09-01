72f6fc6dac1667287a45a51023ac52321050e287
Coral Fustero <coral.fustero@bih-charite.de>
2023-09-01 14:45:55 +0200

    Fix filepaths in rmd report.

--- scripts/01_preprocess_render.R
+++ scripts/01_preprocess_render.R
@@ -4,24 +4,37 @@ print(outs)
 directory = snakemake@params$directory
 print(directory)
 sample_id = unlist(sapply(strsplit(unlist(sapply(strsplit(outs, directory),`[`, 2, simplify=FALSE)), "/cellranger_count/outs"), `[`, 1, simplify=FALSE))
+print(sample_id)
 reference = snakemake@params$reference
+print(reference)
 ncount_atac_max = snakemake@params$ncount_atac_max
+print(ncount_atac_max)
 ncount_rna_max = snakemake@params$ncount_rna_max
+print(ncount_rna_max)
 ncount_atac_min = snakemake@params$ncount_atac_min
+print(ncount_atac_min)
 ncount_rna_min = snakemake@params$ncount_rna_min
+print(ncount_rna_min)
 nuclesome_signal = snakemake@params$nuclesome_signal
+print(nuclesome_signal)
 tss_enrichment = snakemake@params$tss_enrichment
+print(tss_enrichment)
 percent_mt = snakemake@params$percent_mt
+print(percent_mt)
 min_depth = snakemake@params$min_depth
+print(min_depth)
 n_cells_conf_detected = snakemake@params$n_cells_conf_detected
+print(n_cells_conf_detected)
 strand_correlation = snakemake@params$strand_correlation
+print(strand_correlation)
 min_cell_var = snakemake@params$min_cell_var
-
+print(min_cell_var)
 message(paste0("Analysing sample ", sample_id))
+message(paste0("File path: ", file.path(directory, sample_id,"signac", paste0('01_preprocessing_', sample_id,'.html'))))
 
 message("Rendering analysis report: 01 sample pre-processing")
-render_report = function(sample) {
-  rmarkdown::render('01_pre-process.Rmd',
+#render_report = function(sample) {
+  rmarkdown::render('scripts/01_pre-process.Rmd',
   params = list(sample = sample_id, 
                 outs = outs, 
                 directory = directory,
@@ -37,7 +50,7 @@ render_report = function(sample) {
                 n_cells_conf_detected = n_cells_conf_detected, 
                 strand_correlation = strand_correlation, 
                 min_cell_var = min_cell_var),
-  output_file = file.path(directory, "signac", paste0('01_preprocessing_', sample_id,'.html'))
-)}
+  output_file = file.path(directory, sample_id,"signac", paste0('01_preprocessing_', sample_id,'.html')))
+#}
 
 
